61139b84562c4a16460525e592cc4b680ef48dcc
jakoch <jakoch@web.de>
2015-01-25 15:07:20 +0100

    formatting

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -8,7 +8,7 @@ class Extensions_Phantom_Tests_BasicTest extends PHPUnit_Extensions_PhantomTestC
         $html .= '<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>';
         $html .= '</body></html>';
 
-        // jQuery() or $() functions will only be defined if they are already loaded into the current document
+        // jQuery() or $() functions will only be defined, if they are already loaded into the current document
         $this->phantom->assertTrue("h1 exists", "$('h1').length == 1");
         $this->phantom->assertTrue("h1 value is Hi", "$('h1').html() == 'Hi'");
         
